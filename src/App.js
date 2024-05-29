fc7717ad0ae9fc0fa7da3f6b9dbb86082d3994be
6xiaowu9 <736518585@qq.com>
2024-05-29 09:44:50 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import { CrazyDiceSDK } from 'crazydice-sdk'
 function App() {
+  const crazydice = 'http://localhost:3000'
+  const initSDK = () => {
+    window.crazyDiceSDK = new CrazyDiceSDK({
+      appId: '1234567890',
+      name: 'Heart of the Brave',
+      logo: '',
+      gameLink: '',
+      version: '1.0.0',
+    })
+    console.log('init success! ', window.crazyDiceSDK)
+  }
+  // 在iframe中接收来自主页面的消息
+  window.addEventListener('message', (event) => {
+    if (event.origin !== crazydice) {
+      return // 只接受来自可信源的消息
+    }
+    console.log('game Message from parent:', event.data)
+  })
+  const sendMsg = () => {
+    window.parent.postMessage('game Hello from iframe', '*')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <button onClick={initSDK}>初始化</button>
+          <button onClick={sendMsg}>通信</button>
+        </li>
+      </ul>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
